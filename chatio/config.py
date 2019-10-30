678d8655875c397a06975137d9c4f83628ee7a53
subaquatic-pierre <subuatic.pierre@gmail.com>
2019-10-30 11:22:57 +0400

    config file

--- chatio/config.py
+++ chatio/config.py
@@ -1,15 +1,8 @@
 import os, json
 
 
-with open('/home/pierre/etc/cs50w/project2/config.json') as config_file:
+with open('/home/pierre/Courses/cs50w/projects/project2/config.json') as config_file:
     config = json.load(config_file)
 
 class Config():
     SECRET_KEY = config.get("SECRET_KEY")
-
-
-
-
-
-
-
