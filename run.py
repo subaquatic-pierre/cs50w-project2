9672c4fd0c41d552cbcb3f27d7b9eb8b002c8639
Pierre du Toit <subaquatic.pierre@gmail.com>
2023-03-07 14:30:55 +0400

    Update hosts

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from chatio import app, socketio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app, host=os.getenv("FLASK_HOST"), port=os.getenv("FLASK_PORT"))
